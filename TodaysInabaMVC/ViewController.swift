0867d5a36ddd2500dc026767950c7a50007c8beb
深瀬 貴将 <fukase@arsaga.jp>
2020-09-27 22:58:37 +0900

    日付と名前両方入力完了で、次画面へ遷移

--- TodaysInabaMVC/ViewController.swift
+++ TodaysInabaMVC/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        dateTextField.delegate = self
+        dateTextField.returnKeyType = .done
+        nameTextField.delegate = self
+        nameTextField.returnKeyType = .done
     }
 
 
+}
+
+extension ViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        if !(dateTextField.text?.isEmpty ?? true) && !(nameTextField.text?.isEmpty ?? true) {
+            print("日付と名前両方の入力が完了しました")
+            
+            let storyboard: UIStoryboard = UIStoryboard(name: "ResultViewController", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
+            vc.modalPresentationStyle = .overCurrentContext
+            self.present(vc, animated: true)
+        }
+        
+        return true
+    }
 }
 
