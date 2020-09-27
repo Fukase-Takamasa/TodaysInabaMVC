8b2d387a25a66d46547b4b0a4cae8763fc213cc6
深瀬 貴将 <fukase@arsaga.jp>
2020-09-27 23:09:04 +0900

    結果画面のレイアウト調整　アウトレット接続

--- TodaysInabaMVC/ResultViewController.swift
+++ TodaysInabaMVC/ResultViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class ResultViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buttonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
