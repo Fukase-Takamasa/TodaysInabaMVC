7580411171ab5a6cb0d24d4cc32329ef77ff660a
深瀬 貴将 <fukase@arsaga.jp>
2020-09-29 11:20:16 +0900

    コード整理

--- TodaysInabaMVC/GoogleData.swift
+++ TodaysInabaMVC/GoogleData.swift
@@ -8,22 +8,18 @@
 
 import Foundation
 
-//Google画像検索APIの取得データをパースするための構造体　ルート
 struct GoogleData: Codable {
     var items:[Items]
-    
-    struct Items: Codable {
-        //タイトル
-        var title: String
-        //画像のURL（リクエストパラメータでsearchTypeをimageに指定していると"link"に記事URLではなく画像のURLが返ってくる。
-        var link: String
-        //記事のURL
-        var image: Url
-        
-        struct Url: Codable {
-            var contextLink: String
-        }
-    }
+}
+
+struct Items: Codable {
+    var title: String
+    var link: String //画像のURL（リクエストパラメータでsearchTypeをimageに指定していると"link"に記事URLではなく画像のURLが返ってくる。
+    var image: Url //記事のURL
+}
+
+struct Url: Codable {
+    var contextLink: String
 }
 
 
