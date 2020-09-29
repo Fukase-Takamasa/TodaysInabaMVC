86b8d72be93af8632622555feb8b6419142c8fae
深瀬 貴将 <fukase@arsaga.jp>
2020-09-29 11:16:07 +0900

    通信中のインジケータを追加

--- TodaysInabaMVC/ViewController.swift
+++ TodaysInabaMVC/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     var toolBar = UIToolbar()
     var datePicker = UIDatePicker()
 
+    
     @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     
@@ -39,16 +40,19 @@ class ViewController: UIViewController {
             .CustomSearch(
                 query: "ちみたん" + randomQuery.randomElement()!,
                 startIndex: Int.random(in: 1...10))) { (result) in
+            
+            HUD.hide()
+            
             switch result {
             case let .success(moyaResponse):
                 do {
-                    let _googleData = try! JSONDecoder().decode(GoogleData.self, from: moyaResponse.data)
+                    let googleData = try! JSONDecoder().decode(GoogleData.self, from: moyaResponse.data)
                                         
                     let storyboard: UIStoryboard = UIStoryboard(name: "ResultViewController", bundle: nil)
                     let vc = storyboard.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
                     vc.modalPresentationStyle = .overCurrentContext
                     
-                    vc.resultImageUrl = _googleData.items[Int.random(in: 0...9)].link
+                    vc.resultImageUrl = googleData.items[Int.random(in: 0...9)].link
                     
                     self.present(vc, animated: true)
                 }catch {
