626e0cf5c6bd9b9013fb711bebd9c5dd0e917ab8
深瀬 貴将 <fukase@arsaga.jp>
2020-09-29 14:50:46 +0900

    SwiftUI版のKFを試用。いらすとやペンギンURL画像をVGrid表示

--- TodaysInabaMVC/HistoryView.swift
+++ TodaysInabaMVC/HistoryView.swift
@@ -6,14 +6,18 @@
 //
 
 import SwiftUI
+import struct Kingfisher.KFImage
 
 struct HistoryView: View {
     
     var body: some View {
-        LazyVGrid(columns: Array(repeating: GridItem(), count: 2), spacing: 0) {
-            ForEach(0..<12, id: \.self) { index in
-                Text("\(index)")
-                    .frame(width: 100, height: 100, alignment: .center)
+        ScrollView(.vertical) {
+            LazyVGrid(columns: Array(repeating: GridItem(), count: 2), spacing: 0) {
+                ForEach(0..<100, id: \.self) { index in
+                    KFImage(URL(string: "https://1.bp.blogspot.com/-_CVATibRMZQ/XQjt4fzUmjI/AAAAAAABTNY/nprVPKTfsHcihF4py1KrLfIqioNc_c41gCLcBGAs/s400/animal_chara_smartphone_penguin.png")!)
+                        .resizable()
+                        .frame(width: 100, height: 100, alignment: .center)
+                }
             }
         }
     }
